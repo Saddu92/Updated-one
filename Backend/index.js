f50d5a0f64827c1e9e5428e352758776c94e64d1
Shaikh Mohammad Saad <shaikhmdsaad92@gmail.com>
2026-02-07 12:35:19 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -63,9 +63,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       process.env.FRONTEND_URL,
-     " https://syncfleet-teal.vercel.app/",
+     "https://syncfleet-teal.vercel.app/",
     ],
     methods: ["GET", "POST"],
     credentials: true,
